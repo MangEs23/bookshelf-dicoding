ddd6f3f6fec6e9034534b8e1c7f8735bd419958b
Mang Es <79657835+MangEs23@users.noreply.github.com>
2023-09-23 13:02:41 +0700

    Update books.js

--- src/books.js
+++ src/books.js
@@ -1,33 +1,3 @@
-const books = [
-  // {
-  //   'id': '1',
-  //   'name': 'Buku A',
-  //   'publisher': 'Dicoding Indonesia',
-  // },
-  // {
-  //   'id': '2',
-  //   'name': 'Buku B',
-  //   'publisher': 'Dicoding Indonesia',
-  // },
-  // {
-  //   'id': '3',
-  //   'name': 'Buku C',
-  //   'publisher': 'Dicoding Indonesia',
-  // },
-];
-
-// const condition = 'Dicoding';
-
-// const filteredBooks = books.filter((book) => book.name == condition || book.publisher.includes(condition));
-
-// const allBooks = filteredBooks.map((book) => {
-//   return {
-//     id: book.id,
-//     name: book.name,
-//   };
-// });
-
-// console.log(allBooks);
-
+const books = [];
 
 module.exports = books;
